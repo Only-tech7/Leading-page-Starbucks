ab4535a339edaa3844e65a22259b0869cb5ea5c8
Only-tech7 <theuzztrapper@gmail.com>
2026-04-08 17:17:56 -0300

    Atualiza layout e estilos da landing page

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ mobileBtn.addEventListener('click', () => {
     icon.classList.toggle('fa-times');
     icon.classList.toggle('fa-bars');
 })
+
+window.addEventListener('scroll', () => {
+const header = document.getElementById('header');
+
+//com esse comando estou dizendo que se (if) o meu scroll for maior que 0, ou seja, se eu tiver rolado a página, ele vai adicionar a classe 'scrolled' no meu header, caso contrário (else), ele vai remover a classe 'scrolled' do meu header.
+if(window.scrollY > 0) {
+   header.style.boxShadow = '0 4px 10px rgba(0,0,0,0.1)';
+} else {
+    header.style.boxshadow = 'none';
+}
+})
